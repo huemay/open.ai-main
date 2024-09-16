b5fa3f1f2057677d97240d5ef9742578e3c3c778
huemay <luisgabrielguterres11@gmail.com>
2024-09-16 03:59:09 -0300

    11

--- backend/src/services/HubServices/HubMessageListener.ts
+++ backend/src/services/HubServices/HubMessageListener.ts
@@ -131,7 +131,7 @@ const HubMessageListener = async (
     const ticket = await FindOrCreateTicketService(
       contact,
       whatsappId: whatsapp.id!,
-      unreadMessages,
+      unreadMessages
     );
 
     // const ticket = await CreateOrUpdateTicketService({
