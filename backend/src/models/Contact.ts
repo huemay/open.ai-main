afe35c9bfdac83b111b6e7885b2de1ddb9f38845
huemay <luisgabrielguterres11@gmail.com>
2024-09-16 01:26:06 -0300

    luisgabrileguterres11@gmail.com

---
+++ backend/src/models/Contact.ts
@@ -0,0 +1,82 @@
+import {
+  Table,
+  Column,
+  CreatedAt,
+  UpdatedAt,
+  Model,
+  PrimaryKey,
+  AutoIncrement,
+  AllowNull,
+  Unique,
+  Default,
+  HasMany,
+  ForeignKey,
+  BelongsTo
+} from "sequelize-typescript";
+import ContactCustomField from "./ContactCustomField";
+import Ticket from "./Ticket";
+import Company from "./Company";
+import Schedule from "./Schedule";
+
+@Table
+class Contact extends Model<Contact> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @Column
+  name: string;
+
+  @AllowNull(true)
+  @Unique
+  @Column
+  number: string;
+
+  @AllowNull(false)
+  @Default("")
+  @Column
+  email: string;
+
+  @Default("")
+  @Column
+  profilePicUrl: string;
+
+  @Default(false)
+  @Column
+  isGroup: boolean;
+
+  @CreatedAt
+  createdAt: Date;
+
+  @UpdatedAt
+  updatedAt: Date;
+
+  @HasMany(() => Ticket)
+  tickets: Ticket[];
+
+  @Column
+  messengerId: string;
+
+  @Column
+  instagramId: string;
+
+  @HasMany(() => ContactCustomField)
+  extraInfo: ContactCustomField[];
+
+  @ForeignKey(() => Company)
+  @Column
+  companyId: number;
+
+  @BelongsTo(() => Company)
+  company: Company;
+
+  @HasMany(() => Schedule, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+    hooks: true
+  })
+  schedules: Schedule[];
+}
+
+export default Contact;
